cf12c31fd13ff0cedaf0aaf44d05730dc12eda9a
Hack Reactor Students <james.yothers@gmail.com>
2014-06-16 15:10:04 -0700

    Implement basic chatterbox send()

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,43 @@
 // YOUR CODE HERE:
 
+var testMsg = {
+  'username': 'JamesYothers',
+  'text': 'I am James',
+  'roomname': '4chan'
+};
+
+var app = {};
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function(data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function(data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
